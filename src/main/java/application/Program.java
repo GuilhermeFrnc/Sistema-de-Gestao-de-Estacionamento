6a489e3ae42fc935335c484e6508a9293220803e
Guilherme Franco <guilhermefrancodeoliveiraa@gmail.com>
2024-08-30 19:24:05 -0300

    Adding business rules to register monthly members

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,20 +1,48 @@
 package application;
 
-import model.dao.DaoFactory;
-import model.dao.MonthlyDAO;
-import model.entities.Monthly;
-import model.entities.VehicleCategory;
-import model.entities.VehicleType;
+import service.MonthlyService;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
-        Long existingMonthly = DaoFactory.checkMonthy("1234567");
+        Scanner sc = new Scanner(System.in);
+        int cont =1;
 
-        System.out.println(existingMonthly);
+        while (cont!= 0){
+            System.out.println("BEM VINDO AO ESTACIONAMENTO!");
+            System.out.println("----------------------------");
+            System.out.println("Oque deseja fazer:");
+            System.out.println("(1) Cadastrar Mensalista.");
+            System.out.println("(0) Fechar o Sitema.");
+            cont= sc.nextInt();
 
-        MonthlyDAO dao = DaoFactory.createMonthyDao();
-        Monthly monthly = new Monthly("4334hgf", VehicleType.CARRO, VehicleCategory.MENSALISTA);
-        dao.insert(monthly);
+            switch (cont){
+                case 1:
+                    System.out.println("Qual a placa do Veículo: ");
+                    String plate = sc.next();
+
+                    System.out.println("Qual tipo do Veículo:");
+                    System.out.println("(1) Carro.");
+                    System.out.println("(2) Moto.");
+                    int model = sc.nextInt();
+
+                    MonthlyService monthlyService = new MonthlyService();
+
+                    try {
+                        monthlyService.registerMonthly(plate, model);
+                        System.out.println("Mensalista cadastrado com sucesso.");
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Erro ao cadastrar mensalista: " + e.getMessage());
+                    }
+                    break;
+
+                case 0:
+                    break;
+            }
+
+        }
+
     }
 }
--- src/main/java/service/MonthlyService.java
+++ src/main/java/service/MonthlyService.java
@@ -1,6 +1,34 @@
 package service;
 
+import model.dao.DaoFactory;
+import model.dao.MonthlyDAO;
+import model.entities.Monthly;
+import model.entities.VehicleCategory;
+import model.entities.VehicleType;
+
 public class MonthlyService {
+    public void registerMonthly(String plate, int model) {
+        if (plate.length() < 7 || plate.length() > 8) {
+            throw new IllegalArgumentException("Placa inválida");
+        }
 
+        VehicleType vehicleType;
+        if (model == 1) {
+            vehicleType = VehicleType.CARRO;
+        } else if (model == 2) {
+            vehicleType = VehicleType.MOTO;
+        } else {
+            throw new IllegalArgumentException("Tipo de veículo inválido");
+        }
 
+        Long existingMonthly = DaoFactory.checkMonthy(plate);
+        MonthlyDAO dao = DaoFactory.createMonthyDao();
+
+        if (existingMonthly == null) {
+            Monthly monthly = new Monthly(plate, vehicleType, VehicleCategory.MENSALISTA);
+            dao.insert(monthly);
+        } else {
+            throw new IllegalArgumentException("Esse veiculo ja e mensalista");
+        }
+    }
 }
